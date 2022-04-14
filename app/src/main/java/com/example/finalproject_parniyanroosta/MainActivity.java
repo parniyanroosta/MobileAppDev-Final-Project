94afd0d15afa0f6e2c6af62e6efdae931b062f08
parniyan <parniyan.roosta@gmail.com>
2022-04-14 16:41:51 -0600

    Set menu, icons and about me page

--- app/src/main/java/com/example/finalproject_parniyanroosta/MainActivity.java
+++ app/src/main/java/com/example/finalproject_parniyanroosta/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +17,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-public void StartPaintingActivity(View view) {
+    public void StartPaintingActivity(View view) {
         Intent intent = new Intent(MainActivity.this, Category_Painting.class);
         startActivity(intent);
-}
+    }
 
     public void StartPotteryActivity(View view) {
         Intent intent = new Intent(MainActivity.this, Category_Pottery.class);
@@ -32,5 +35,42 @@ public void StartPaintingActivity(View view) {
     public void StartEmbroideryActivity(View view) {
         Intent intent = new Intent(MainActivity.this, Category_Embroidery.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        //TODO
+        // to save the page info when the device is rotated-------@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate the menu, this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //handle action bar item clicks here. The action bar will automatically handle clicks
+        // on the home/up button so long as a parent activity is specified in AndroidManifest.xml
+        switch ( item.getItemId()){
+            case R.id.action_about:
+                Intent intent = new Intent(this, AboutMe.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_contact:
+                //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                return true;
+            case R.id.action_login:
+                //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
